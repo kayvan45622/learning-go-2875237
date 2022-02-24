2794e2b204e10d3213a87e2d44fe851fffbd7e27
kayvan45622 <kevinkhalafi@gmail.com>
2022-02-24 09:14:26 -0800

    test

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("A simple calculator")
+	//fmt.Println("A simple calculator")
+
+	//panic("This is my error message")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter first number:")
+	numFirst, _ := reader.ReadString('\n')
+	aFloat, err1 := strconv.ParseFloat(strings.TrimSpace(numFirst), 64)
+
+	if err1 != nil {
+		fmt.Println(err1)
+		panic("You first number is invalud!!")
+	}
+
+	fmt.Print("Enter second number:")
+	numSecond, _ := reader.ReadString('\n')
+
+	bFloat, err2 := strconv.ParseFloat(strings.TrimSpace(numSecond), 64)
+
+	if err2 != nil {
+		fmt.Println(err2)
+		panic("You second number is invalud!!")
+	}
+
+	sumNum := aFloat + bFloat
+	sumNum = math.Round(sumNum*100) / 100
+
+	fmt.Println("A simple calculator: ", sumNum)
 }
